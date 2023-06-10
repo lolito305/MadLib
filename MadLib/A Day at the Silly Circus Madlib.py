9c5dacfe1129d7fd7f206fa41830ae6fea6d037a
lolito305 <74833887+lolito305@users.noreply.github.com>
2023-06-10 11:15:30 -0700

    Add files via upload

---
+++ MadLib/A Day at the Silly Circus Madlib.py
@@ -0,0 +1,40 @@
+Adjective = input("Enter an adjective: ")
+Animal_plural = input("Enter an animal (plural): ")
+Adjective2 = input("Enter an adjective: ")
+Type_of_Food_plural = input("Enter a type of food (plural): ")
+Verb_ending_in_ing = input("Enter a verb ending in -ing: ")
+Adjective3 = input("Enter an adjective: ")
+Noun = input("Enter a noun: ")
+Verb_past_tense = input("Enter a verb (past tense): ")
+Adjective4 = input("Enter an adjective: ")
+Type_of_Dance = input("Enter a type of dance: ")
+
+madlib = f"Once upon a time, in a {Adjective} town called Jollyville, there was a circus unlike any other. \
+This circus was known for its {Adjective2} performances and mischievous {Animal_plural}. The \
+The townspeople gathered with excitement, ready to be entertained. \
+\
+The circus tent was decorated with colorful banners and smelled of freshly baked {Type_of_Food_plural}. As \
+the audience settled into their seats, the spotlight illuminated the ringmaster, who was \
+{Verb_ending_in_ing} the air with a baton while wearing a {Adjective3} expression. The crowd erupted in \
+laughter, unsure if it was part of the act or a comical accident. \
+\
+Suddenly, from above, a {Adjective4} acrobat swung down on a trapeze, gracefully flipping through \
+the air. The audience gasped in awe. The acrobat, having {Verb_past_tense} well the night before, \
+performed daring somersaults and flips that left everyone astounded. \
+\
+But the real showstopper was when the {Noun} took the stage. With their {Adjective2} antics and \
+oversized shoes, they began dancing the {Type_of_Dance}. The crowd couldn't resist joining in, \
+hopping and spinning along with the hilarious {Noun}. Laughter echoed through the circus \
+tent as everyone tried their best to keep up with the silly dance moves.\
+\
+The {Animal_plural}, known for their mischievous nature, couldn't resist joining the fun. They swung \
+from the trapeze, attempting to imitate the {Type_of_Dance}, which only made the \
+audience laugh harder. The {Noun}, surprised but delighted, incorporated the {Animal_plural} into their routine, turning the entire performance into \
+a riot of laughter and chaos. \
+\
+The day at the Silly Circus ended with smiles and giggles as the townspeople left, still \
+chuckling at the memory of the {Adjective2} {Animal_plural} attempting the {Type_of_Dance}. It was a day \
+filled with unexpected surprises, reminding everyone that sometimes the most peaceful \
+moments can lead to the silliest adventures."
+
+print(madlib)
---
+++ MadLib/The Hilarious Haunted House.py
@@ -0,0 +1,38 @@
+Adjective = input("Enter an adjective: ")
+Noun = input("Enter a noun: ")
+Adjective2 = input("Enter an adjective: ")
+Verb = input("Enter a verb: ")
+Noun2 = input("Enter a noun: ")
+Adjective3 = input("Enter an adjective: ")
+Verb_ending_in_ing = input("Enter a verb ending in ing: ")
+Adjective4 = input("Enter an adjective: ")
+Verb2 = input("Enter a verb: ")
+Noun3 = input("Enter a noun: ")
+
+print("Welcome to the Hilarious Haunted House!")
+
+madlib = f"Once upon a time, in a {Adjective} haunted house, there was a mysterious {Noun} that had a \
+{Adjective2} aura. The inhabitants of the house were known for their {Verb} sense of \
+humor. As visitors walked through the rooms, they couldn't help but laugh at the silly \
+surprises that awaited them. \
+\
+In one room, there was a peculiar {Noun2} that had a {Adjective3} expression painted on it. People \
+couldn't resist making faces at their reflections, which resulted in fits of laughter. The \
+{Noun2} seemed to enjoy the attention, as if it were {Verb_ending_in_ing} tears of joy. \
+\
+Another room had {Adjective4} portraits that seemed to come alive, their eyes following anyone \
+who walked by. Visitors found themselves {Verb2} away in mock terror, even though they \
+knew it was all in good fun. The haunted house was filled with giggles and screams of \
+laughter. \
+\
+In the final room, there was a life-sized animatronic {Noun3} that would jump out and startle \
+people. However, instead of fear, it elicited BURSTS of laughter as it playfully wagged its \
+tail and licked their faces. \
+\
+The hilarious haunted house became a popular attraction in the area, with people coming \
+from far and wide to experience the unique combination of {Adjective} scares and \
+uncontrollable laughter. It became a place where both children and adults could let loose \
+and enjoy the whimsical world of the haunted house."
+
+print(madlib)
+
